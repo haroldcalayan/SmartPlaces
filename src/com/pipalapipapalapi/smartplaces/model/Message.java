b75b8c5a2a4f577a9c7361d69eb62afd32407aa1
haroldcalayan@yahoo.com <haroldcalayan@yahoo.com>
2014-12-06 12:01:47 +0800

    add fields in models

--- src/com/pipalapipapalapi/smartplaces/model/Message.java
+++ src/com/pipalapipapalapi/smartplaces/model/Message.java
@@ -1,5 +1,83 @@
 package com.pipalapipapalapi.smartplaces.model;
 
 public class Message {
+	
+	private int id;
+	
+	private int status;
+	
+	private double latitude;
+	
+	private double longitude;
+	
+	private int trigger;
+	
+	private String recipientNumber;
+	
+	private String message;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getTrigger() {
+		return trigger;
+	}
+
+	public void setTrigger(int trigger) {
+		this.trigger = trigger;
+	}
+
+	public String getRecipientNumber() {
+		return recipientNumber;
+	}
+
+	public void setRecipientNumber(String recipientNumber) {
+		this.recipientNumber = recipientNumber;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", status=" + status + ", latitude="
+				+ latitude + ", longitude=" + longitude + ", trigger="
+				+ trigger + ", recipientNumber=" + recipientNumber
+				+ ", message=" + message + "]";
+	}
 
 }
--- src/com/pipalapipapalapi/smartplaces/model/Reminder.java
+++ src/com/pipalapipapalapi/smartplaces/model/Reminder.java
@@ -2,4 +2,71 @@ package com.pipalapipapalapi.smartplaces.model;
 
 public class Reminder {
 
+	private int id;
+	
+	private int status;
+	
+	private double latitude;
+	
+	private double longitude;
+	
+	private int trigger;
+	
+	private String message;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getTrigger() {
+		return trigger;
+	}
+
+	public void setTrigger(int trigger) {
+		this.trigger = trigger;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Toggle [id=" + id + ", status=" + status + ", latitude="
+				+ latitude + ", longitude=" + longitude + ", trigger="
+				+ trigger + ", message=" + message + "]";
+	}
+
 }
