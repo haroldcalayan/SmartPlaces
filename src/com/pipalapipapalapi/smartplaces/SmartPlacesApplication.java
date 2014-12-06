1e07c35fd8e80d46163361e6e1ca265013c4a2cb
haroldcalayan@yahoo.com <haroldcalayan@yahoo.com>
2014-12-06 10:27:50 +0800

    add application class

---
+++ src/com/pipalapipapalapi/smartplaces/SmartPlacesApplication.java
@@ -0,0 +1,18 @@
+package com.pipalapipapalapi.smartplaces;
+
+import android.app.Application;
+import android.content.Context;
+
+public class SmartPlacesApplication extends Application {
+
+  private static Context mContext;
+
+  @Override public void onCreate() {
+    super.onCreate();
+    mContext = getApplicationContext();
+  }
+
+  public static Context getContext() {
+    return mContext;
+  }
+}
